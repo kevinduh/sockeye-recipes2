a6a0863f3581854a99f1104af2b76d2769e5a626
Kevin Duh <kevinduh@cs.jhu.edu>
2021-06-02 11:23:14 -0400

    merge and update for cuda11.2

---
+++ install/install_sockeye_gpu_latestcuda.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+SOCKEYE_COMMIT=ba8f849804e99c9fac3ff31ce4dac3758fcda95b # 2.3.16 (sockeye:master)
+
+# Get this version of sockeye
+rootdir="$(readlink -f "$(dirname "$0")/../")"
+cd $rootdir
+git submodule init
+git submodule update --recursive --remote sockeye
+cd sockeye
+git checkout $SOCKEYE_COMMIT
+cd ..
+
+$rootdir/install/install_sockeye_custom.sh -s $rootdir/sockeye -e sockeye2_latestcuda
